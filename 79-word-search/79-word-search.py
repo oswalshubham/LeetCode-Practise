592816e458370170bd055bd5af040cfa9ba2dc8c
oswalshubham <95557338+oswalshubham@users.noreply.github.com>
2022-03-09 16:50:02 -0500

    Time: 6660 ms (65.42%), Space: 13.9 MB (68.07%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,15 +1,17 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        if len(word) == 0:
+            return True
         def dfs(i,j,index,visited = []):
             if index == len(word):
                 return True
             if i>=0 and i < len(board) and j>=0 and j<len(board[0]) and board[i][j] == word[index]:
                 temp = board[i][j]
                 board[i][j] = "#"
-                right = dfs(i,j+1,index+1,visited)
-                left = dfs(i,j-1, index+1,visited)
-                up = dfs(i-1,j,index+1,visited)
-                down = dfs(i+1,j,index+1,visited)
+                right = dfs(i,j+1,index+1)
+                left = dfs(i,j-1, index+1)
+                up = dfs(i-1,j,index+1)
+                down = dfs(i+1,j,index+1)
                 
                 res = left or right or up or down
                 board[i][j] = temp
@@ -22,8 +24,7 @@ class Solution:
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == word[0]:
-                    visited = []
-                    result = dfs(i,j,0, visited)
+                    result = dfs(i,j,0)
                     if result == True:
                         return True
                     
