8019bfc9e8800bbc2392993181a963549bdb7a22
oswalshubham <95557338+oswalshubham@users.noreply.github.com>
2023-08-21 17:20:15 -0500

    Time: 98 ms (93.08%), Space: 19.3 MB (73.5%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -3,64 +3,42 @@ class Solution:
 
         stack = []
         num = 0
-        flag = False
+        prev_oper = "+"
+
+        s = s+ "+"
 
         '''
-        25+15*5+50/2
-        [25,+,75,+,25]
+        25-15*5+50/2+
+        [25,-75,25]
         '''
         for char in s:
             if char.isdigit():
                 num = num*10+int(char)
 
             elif char in ["+", "-", "*", "/"]:
-                if flag:
-                    sign = stack.pop()
-                    a = stack.pop()
+                if prev_oper == "+":
+                    stack.append(num)
 
-                    if sign == "*":
-                        num = a*num
-                    else:
-                        num = a//num
+                elif prev_oper == "-":
+                    stack.append(-num)
 
-                    flag = False
-                
-                if char in ["*", "/"]:
-                    flag = True
-                
-                stack.append(num)
-                if char == "+":
-                    stack.append(1)
-                elif char == "-":
-                    stack.append(-1)
-                else:
-                    stack.append(char)
+                elif prev_oper == "*":
+                    top = stack.pop()
+                    stack.append(top*num)
 
+                elif prev_oper == "/":
+                    top = stack.pop()
+                    stack.append(math.trunc(top/num))
+
+                prev_oper = char
                 num = 0
 
-        if flag:
-            sign = stack.pop()
-            a = stack.pop()
+        return sum(stack)
 
-            if sign == "*":
-                num = a*num
-            else:
-                num = a//num
+                
+                
 
-        stack.append(num)
-
-        stack.reverse()
-        while len(stack) > 1:
-                a = stack.pop()
-                sign = stack.pop()
-                b = stack.pop()
-
-                answer = a+b*sign
-
-                stack.append(answer)
-
-        return stack[-1]
-
+                
 
 
 
