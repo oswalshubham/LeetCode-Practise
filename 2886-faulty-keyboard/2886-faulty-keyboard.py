fa8e3ecc7b5ceee61cfbb5d8300d517224dca955
oswalshubham <95557338+oswalshubham@users.noreply.github.com>
2023-08-23 20:37:09 -0500

    Time: 46 ms (76.82%), Space: 16.2 MB (71.1%) - LeetHub

---
+++ 2886-faulty-keyboard/2886-faulty-keyboard.py
@@ -0,0 +1,12 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+
+        result = ""
+
+        for char in s:
+            if char == "i":
+                result = result[::-1]
+            else:
+                result+=char
+
+        return result
