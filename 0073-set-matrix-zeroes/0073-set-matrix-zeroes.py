3289a0de1c8cfa2de809caee293b29349fff65b2
oswalshubham <95557338+oswalshubham@users.noreply.github.com>
2023-09-09 13:58:40 -0500

    Time: 104 ms (98.42%), Space: 17.4 MB (17.22%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -5,19 +5,41 @@ class Solution:
         """
         m = len(matrix)
         n = len(matrix[0])
+        first_row = False
+        first_col = False
 
-        row = [1 for _ in range(m)]
-        col = [1 for _ in range(n)]
-
+        #checking if first row has zero
+        for j in range(n):
+            if matrix[0][j] == 0:
+                first_row = True
+                break
+        #checking if first column has zero
         for i in range(m):
-            for j in range(n):
+            if matrix[i][0] == 0:
+                first_col = True
+                break
+
+        #starting (1,1) if any element is zero, setting respective index in first row and first col to zero
+        for i in range(1,m):
+            for j in range(1,n):
                 if matrix[i][j] == 0:
-                    row[i] = 0
-                    col[j] = 0
-
-        for i in range(m):
-            for j in range(n):
-                if row[i] == 0 or col[j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        #filling zeroes
+        for i in range(1,m):
+            for j in range(1,n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
+
+        #filling first row if first_row
+        if first_row:
+            for j in range(n):
+                matrix[0][j] = 0
+
+        if first_col:
+            for i in range(m):
+                matrix[i][0] = 0
+
+        
 
         
